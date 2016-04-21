3fbfae2d15c87eff3d79f056362c4945f4b092e2
Jinqiu Deng <deng.jinqiu@gmail.com>
2016-04-20 20:32:02 -0400

    display the message from current user

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,8 +1,9 @@
 class ConversationsController < ApplicationController
   def create
     Rails.logger.debug "Get message " << params[:conversation][:context] << \
-                       " from " << params[<< " to " << get_current_user()
-    render :controller => "users", :action => "index"
+                       " from " << params[:conversation][:selectedUser] << \
+                       " to " << get_current_user()
+    render js: "$('#message-board').append('ddd');"
   end
 end
 
